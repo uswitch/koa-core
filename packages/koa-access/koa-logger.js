d742c742322eabb98d39e7f776ea4056e9018c3e
Dom Charlesworth <dgc336@gmail.com>
2017-10-30 16:13:51 +0000

    ⭐ Initial commit!

---
+++ packages/koa-access/koa-logger.js
@@ -0,0 +1,20 @@
+export default (extraKeys = []) => async (ctx, next) => {
+  const time = new Date()
+
+  /* Wait for request to happen */
+  await next()
+
+  const responseTime = new Date() - time
+  const { length, status } = ctx.response
+  const { method, path, ip: host } = ctx.request
+
+  /* Cast all integers to integers */
+  const res = { responseTime: +responseTime, length: +length, status: +status, time: new Date() }
+  const req = { method, path, time, host, upstream: false }
+
+  const extras = extraKeys instanceof Function
+        ? extraKeys(ctx)
+        : extraKeys.reduce((acc, it) => ctx.state[it] ? { ...acc, [it]: ctx.state[it] } : acc, {})
+
+  ctx.app && ctx.app.emit('access', { req, res, ...extras })
+}
