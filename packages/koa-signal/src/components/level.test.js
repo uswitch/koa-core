4d49ad88228a361f90e9db1721acffb6a1fea9c4
Dom Charlesworth <dgc336@gmail.com>
2018-06-18 16:59:14 +0100

    🔬 Add tests for `level` component

---
+++ packages/koa-signal/src/components/level.test.js
@@ -0,0 +1,15 @@
+/* global describe, beforeEach, it, expect */
+import level from './level'
+
+describe(`koa-signal | level component`, () => {
+  let ctx
+  beforeEach(() => (ctx = { }))
+
+  it(`should return the right format`, () => {
+    expect(level({ label: 'success' })(ctx)).toEqual('success')
+  })
+
+  it(`should add a badge if provided`, () => {
+    expect(level({ label: 'success', badge: '!' })(ctx)).toEqual('! success')
+  })
+})
