a10a86bac286936b970612db4b2664f18171e10c
Dom Charlesworth <dgc336@gmail.com>
2018-06-19 16:11:43 +0100

    ♻️ Allow renaming of the json properties and nested object fetching

--- packages/koa-signal/src/components/json.js
+++ packages/koa-signal/src/components/json.js
@@ -1,9 +1,21 @@
-const path = (obj, path = []) => typeof path === 'string'
-  ? path.split('.').reduce((acc, it) => (acc && acc[it]) ? acc[it] : undefined, obj)
-  : path.reduce((acc, it) => (acc && acc[it]) ? acc[it] : undefined, obj)
+const splitPath = (p = []) => {
+  if (Array.isArray(p)) return [p]
+
+  const [ _, key ] = p.split(':')
+  const path = _.split('.')
+
+  return [ path, key || path.slice(-1)[0] ]
+}
+
+const pathAsObj = (obj, p = []) => {
+  const [ path, key ] = splitPath(p)
+  const val = path.reduce((acc, it) => (acc && acc[it]) ? acc[it] : undefined, obj)
+
+  return val && { [key]: val }
+}
 
 const pluck = (obj, props = []) => props
-  .reduce((acc, it) => path(obj, it) ? { ...acc, [it]: path(obj, it) } : acc, {})
+  .reduce((acc, it) => pathAsObj(obj, it) ? { ...acc, ...pathAsObj(obj, it) } : acc, {})
 
 /* Config and then passed Koa ctx & msg */
 export default (config = {}) => (ctx, extras = {}) => {
