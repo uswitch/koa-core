04575a0df4fcb715bd7105b34ccda085dee07a2f
Dom Charlesworth <dgc336@gmail.com>
2018-06-20 10:36:43 +0100

    Remove the build version of Koa core

---
+++ packages/koa-core/merge.js
@@ -0,0 +1,12 @@
+/* https://30secondsofcode.org/object#merge */
+module.exports = {
+  merge: (...objs) =>
+    [...objs].reduce(
+      (acc, obj) =>
+        Object.keys(obj).reduce((a, k) => {
+          acc[k] = acc.hasOwnProperty(k) ? [].concat(acc[k]).concat(obj[k]) : obj[k]
+          return acc
+        }, {}),
+      {}
+    )
+}
