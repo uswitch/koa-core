67649b5d889070da0485410c8b846ae0d0d7c50e
Dom Charlesworth <dgc336@gmail.com>
2018-07-10 14:27:00 +0100

    Tidy up Meter to use labels rather than individual metrics

--- packages/koa-prometheus/src/utils/metrics-meter.js
+++ packages/koa-prometheus/src/utils/metrics-meter.js
@@ -1,19 +1,15 @@
 import { camelCase } from './s'
 
-const print = ({help}, name, value) => `
-# HELP ${help}
-# TYPE ${name} gauge
-${name} ${value}`
-
 export const isMetricsMeter = ({ type }) => [ 'Meter' ].includes(type)
 export const printMeter = (meters) => (config) => {
-  const { name } = config
+  const { name, help } = config
   const meter = meters[camelCase(name)]
 
-  return `
-${print(config, name + '_per_minute', meter.oneMinuteRate())}
-${print(config, name + '_per_five_minutes', meter.fiveMinuteRate())}
-${print(config, name + '_per_fifteen_minutes', meter.fifteenMinuteRate())} `
+  return `# HELP ${help}
+# TYPE ${name} gauge
+${name}{decay="1"} ${meter.oneMinuteRate()}
+${name}{decay="5"} ${meter.fiveMinuteRate()}
+${name}{decay="15"} ${meter.fifteenMinuteRate()} `
 }
 
 export default (config, meters) => config.filter(isMetricsMeter).map(printMeter(meters))
