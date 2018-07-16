7807d508f648929c7a089dbbe2d4fbcf4251c5c8
Dom Charlesworth <dgc336@gmail.com>
2018-07-16 14:06:27 +0100

    [core] Update the dependency script to include the latest uswitch modules

--- __scripts__/update-dependencies.js
+++ __scripts__/update-dependencies.js
@@ -3,14 +3,26 @@ const { exec } = require('child_process')
 const execAsync = cmd => new Promise((resolve, reject) => exec(cmd, (err, out) => err ? reject(err) : resolve(out)))
 
 const updates = Object
-      .keys(dependencies)
-      .filter((dep) => dep.startsWith('@uswitch'))
-      .map((dep) => `npm update ${dep}`)
+  .keys(dependencies)
+  .filter((dep) => dep.startsWith('@uswitch'))
+  .map((dep) => `npm update ${dep}`)
 
-console.log(`Beginning updates of ${updates.length} packages`)
+console.log(`Beginning updates of ${updates.length} non '@uswitch' packages`)
 Promise
   .all(updates.map(execAsync))
   .then((out) => {
-    console.log('Updates successful!')
+    console.log(`Updated ${out.length} packages successfully!`)
     console.log(out)
+  })
+
+console.log(`Beginning updates of '@uswitch' packages`)
+
+execAsync('npx lerna ls --json')
+  .then((json) => {
+    Promise.all(
+      JSON
+        .parse(json)
+        .map(({ name, version }) => `npm install ${name}@${version}`)
+        .map(execAsync)
+    ).then(out => console.log(`Updated '@uswitch' packages successfuly!`))
   })
