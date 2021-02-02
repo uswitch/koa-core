18290d3a1ce0490d846a73a9b73a9dad9fa0375d
Phil Helm <github@phelm.co.uk>
2021-02-02 12:02:50 +0000

    import components directly

---
+++ packages/koa-signal/src/components/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'access-log': require('access-log.js'),
+  'access': require('access.js'),
+  'error': require('error.js'),
+  'file-name': require('file-name.js'),
+  'id': require('id.js'),
+  'json': require('json.js'),
+  'level': require('level.js'),
+  'map': require('map.js'),
+  'message': require('message.js'),
+  'raw-json': require('raw-json.js'),
+  'scope': require('scope.js'),
+  'summary': require('summary.js'),
+  'time-diff-init': require('time-diff-init.js'),
+  'time-diff-scope': require('time-diff-scope.js'),
+  'zipkin-level': require('zipkin-level.js'),
+}
