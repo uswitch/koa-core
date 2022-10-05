2b7de5b7aba3896dfcb6adcefece0880a2dcc31b
Sam Egger <segger87@gmail.com>
2022-10-05 12:52:59 +0100

    fix time stamps not working on tracer

--- packages/koa-signal/src/components/index.js
+++ packages/koa-signal/src/components/index.js
@@ -1,18 +1,18 @@
-import id from './id'
-import access from './access'
-import accessLog from './access-log'
-import error from './error'
-import fileName from './file-name'
-import json from './json'
-import level from './level'
-import map from './map'
-import message from './message'
-import rawJson from './raw-json'
-import scope from './scope'
-import summary from './summary'
-import timeDiffInit from './time-diff-init'
-import timeDiffScope from './time-diff-scope'
-import zipkinLevel from './zipkin-level'
+import id from "./id";
+import access from "./access";
+import accessLog from "./access-log";
+import error from "./error";
+import fileName from "./file-name";
+import json from "./json";
+import level from "./level";
+import map from "./map";
+import message from "./message";
+import rawJson from "./raw-json";
+import scope from "./scope";
+import summary from "./summary";
+import timeDiffInit from "./timeDiffInit";
+import timeDiffScope from "./timeDiffScope";
+import zipkinLevel from "./zipkin-level";
 
 export default {
   id,
@@ -29,5 +29,5 @@ export default {
   summary,
   timeDiffInit,
   timeDiffScope,
-  zipkinLevel
-}
+  zipkinLevel,
+};
