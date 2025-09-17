ab8a7859933acccb7c0ec6fde69aadf5b36c6b15
Kristin Wong <kristin.kasavetova@rvu.co.uk>
2025-09-17 16:33:02 +0100

    upgrade lerna

--- packages/koa-signal/src/outputs/outputs.test.js
+++ packages/koa-signal/src/outputs/outputs.test.js
@@ -1,12 +1,14 @@
 /* global describe, it, expect, jest */
-import Signal from './koa-signal'
+import Signal from '../koa-signal'
 
 describe(`koa-signal | outputs`, () => {
   it(`should throw when an invalid output type is defined`, () => {
-    expect(new Signal({
-      outputs: [
-        { type: 'invalid-output-type' }
-      ]
-    })).toThrow()
+    expect(() => {
+      new Signal({
+        outputs: [
+          { type: 'invalid-output-type' }
+        ]
+      })
+    }).toThrow()
   })
 })
